db92fe21e4c528cbc246d6abe81d4b76ff5efafb
Silver Lee-A-Fong <silverlee2000@gmail.com>
2021-07-24 17:40:23 +0200

    Initial project commit

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express'
+import cors from 'cors'
+
+import { WebhookClient } from 'dialogflow-fulfillment'
+
+// intent method imports
+import { fallback } from './intents/fallback'
+import { welcome } from './intents/welcome'
+import { weatherAtLocation } from './intents/weatherAtLocation'
+import { weatherAtTimeAndLocation } from './intents/weatherAtTimeAndLocation'
+
+
+const app = express()
+
+const PORT: number = 8080
+
+app.use(
+    cors({ origin: '*' }),
+    express.json(),
+)
+
+// Map of intent-name to their respective method
+const intents = new Map<string, (agent: any) => void>()
+
+// Set specific intent-name to it's respective method
+intents.set('Default Fallback Intent', fallback)
+intents.set('Default Welcome Intent', welcome)
+intents.set('Weather at Location', weatherAtLocation)
+intents.set('Weather at Time and Location', weatherAtTimeAndLocation)
+
+app.post('/', async (req, res) => {
+    const agent: any = new WebhookClient({ request: req, response: res })
+
+    await agent.handleRequest(intents)
+})
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}!`))
