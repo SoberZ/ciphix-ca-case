8a477c2dff7f8f6d663ae3232bb11070d26aab11
Silver Lee-A-Fong <silverlee2000@gmail.com>
2021-07-25 17:56:45 +0200

    Changed API of weather at location

--- src/intents/weatherAtLocation.ts
+++ src/intents/weatherAtLocation.ts
@@ -1,21 +1,22 @@
 // Intent name: Weather at Location
 
 import axios from 'axios'
+import { apiKey } from '../apiKey'
 
 
 export const weatherAtLocation = async (conv: any) => {
 
-    // Get the city parameter
+    // Get the city parameter.
     const city = conv.parameters.city
 
-    // Obtain the weather from the location
-    return axios.get(`https://goweather.herokuapp.com/weather/${city}`).then(res => {
-            const temp = res.data.temperature
-            const descr = res.data.description
-            const wind = res.data.wind
+    // Obtain the weather from the location using the goweather API.
+    return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then(res => {
+            const descr = res.data.weather[0].description
+            const temp = res.data.main.temp
 
-            conv.add(`In ${city} it's currently ${temp} and ${descr}. The windspeed is ${wind}.`)
-        }).catch(res => {
+            conv.add(`The weather in ${city} is ${descr} with a temperature of ${temp} celsius.`)
+        }).catch(_ => {
             conv.add("Weather data is currently unavailable. Please try again later.")
         })
 }
